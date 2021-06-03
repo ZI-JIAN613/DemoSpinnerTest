95467e7708530a8ab1631ff0498b4bbe42bf2071
20025988 <20025988@myrp.edu.sg>
2021-06-03 11:39:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demospinnertest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spnYesNo;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spnYesNo = findViewById(R.id.spinner);
+        tv = findViewById(R.id.textView);
+
+        spnYesNo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, position + " is selected", Toast.LENGTH_SHORT).show();
+
+                switch (position) {
+                    case 0:
+                        tv.setText("Spinner Item, Yes Selected");
+                        break;
+                    case 1:
+                        tv.setText("Spinner Item, No Selected");
+                        break;
+                }
+
+                /*
+
+                if (position == 0) {
+                    tv.setText("Spinner Item, Yes Selected");
+                } else if (position == 1) {
+                    tv.setText("Spinner Item, No Selected");
+                }
+
+                 */
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
